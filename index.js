49dd7a732235c7741df7c1a0a5fa6023a1b22b12
Jon Kaplon <jon.kaplon1@gmail.com>
2022-02-06 09:36:16 -0800

    commit tree 6d7e9c620636c02c89e13e4b817ddf740c466501

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  loaded();
+});
+
+function loaded() {
+  document.getElementById("text").innerHTML = "This is really cool!";
+}
